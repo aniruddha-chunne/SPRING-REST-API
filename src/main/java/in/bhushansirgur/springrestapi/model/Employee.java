0bee6437e416af65e299c82242ef62c86cf2879a
aniruddha-chunne <achunne3@gmail.com>
2023-09-19 11:07:49 +0530

    18 - getter and setter

--- src/main/java/in/bhushansirgur/springrestapi/model/Employee.java
+++ src/main/java/in/bhushansirgur/springrestapi/model/Employee.java
@@ -45,6 +45,75 @@ public class Employee {
 	
 	@Column(name = "department")
 	private String department;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getAge() {
+		return age;
+	}
+
+	public void setAge(Long age) {
+		this.age = age;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public Employee(Long id, String name, Long age, String location, String email, String department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.location = location;
+		this.email = email;
+		this.department = department;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", location=" + location + ", email=" + email
+				+ ", department=" + department + "]";
+	}
 	
 
 }
